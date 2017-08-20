0ae5c0c1af0508d33fb6fe7c43a8ef76ee19fcc4
Christoph <ignatz@gmail.com>
2017-08-20 18:58:15 -0400

    added some validator tests

--- models.articles.go
+++ models.articles.go
@@ -1,16 +1,16 @@
 package main
 
-type article struct {
-	ID	int `json:"id"`
+type Article struct {
+	ID	int `json:"id" validate:"min=1"`
 	Title string `json:"title" validate:"nonzero"`
 	Content string `json"content" validate:"nonzero"`
 }
 
-var articleList = []article {
-	article{ID: 1, Title: "Article 1", Content: "Article 1 Body"},
-	article{ID: 2, Title: "Article 2", Content: "Article 2 Body"},
+var articleList = []Article{
+	Article{ID: 1, Title: "Article 1", Content: "Article 1 Body"},
+	Article{ID: 2, Title: "Article 2", Content: "Article 2 Body"},
 }
 
-func getAllArticles() []article {
+func getAllArticles() []Article {
 	return articleList
 }
--- models.articles_test.go
+++ models.articles_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"testing"
 	"fmt"
+	"testing"
+	"gopkg.in/validator.v2"
 )
 
 func TestGetAllArticles(t *testing.T) {
@@ -10,5 +11,17 @@ func TestGetAllArticles(t *testing.T) {
 	for count, article := range articles {
 		title := fmt.Sprintf("Article %d", count)
 		article.Title = title
+
+		if errs := validator.Validate(article); errs != nil {
+			t.Fail()
+		}
 	}
 }
+
+func TestValidate_InvalidArticle(t *testing.T) {
+	nur := Article{ID: 0, Title: "Article 0", Content: "Article 1 Body"}
+	if errs := validator.Validate(nur); errs == nil {
+		t.Fail()
+	}
+}
+
